e918a658a15eca5525ae698effbaa53ef5e37815
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-21 18:04:52 +0100

    Import modules alphabetically.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ First use of REST API. """
 
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == "__main__":
     todos_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
