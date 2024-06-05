802af21303428bf821de99dfc05573ef2c79cb57
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-06-05 11:37:53 +0100

    Set allow_redirects option to False.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'ALX by /u/eveshogweyore'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         subreddit_data = response.json()
