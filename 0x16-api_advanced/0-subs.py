c134c4cf861a59f87ffd51f9dcc36d6644fb5ee6
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-06-05 11:33:12 +0100

    Add user header.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,9 @@ import requests
 def number_of_subscribers(subreddit):
     """ Given subreddit, return number of subscribers. """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'ALX by /u/eveshogweyore'}
 
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         subreddit_data = response.json()
